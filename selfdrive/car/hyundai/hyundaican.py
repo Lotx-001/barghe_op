3a41f5f5c9bd4075d8d4d1c64c667053b4f29fab
Lotx-001 <72420846+Lotx-001@users.noreply.github.com>
2021-06-28 19:38:03 +0300

    Update hyundaican.py

--- selfdrive/car/hyundai/hyundaican.py
+++ selfdrive/car/hyundai/hyundaican.py
@@ -12,7 +12,7 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
                   left_lane_depart, right_lane_depart, bus):
   values = copy.copy(lkas11)
   values["CF_Lkas_LdwsSysState"] = sys_state
-  values["CF_Lkas_SysWarning"] = 3 if sys_warning else 0
+  values["CF_Lkas_SysWarning"] = 2 if sys_warning else 0
   values["CF_Lkas_LdwsLHWarning"] = left_lane_depart
   values["CF_Lkas_LdwsRHWarning"] = right_lane_depart
   values["CR_Lkas_StrToqReq"] = apply_steer
@@ -37,7 +37,7 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
     # SysWarning 5 = keep hands on wheel (red)
     # SysWarning 6 = keep hands on wheel (red) + beep
     # Note: the warning is hidden while the blinkers are on
-    values["CF_Lkas_SysWarning"] = 4 if sys_warning else 0
+    values["CF_Lkas_SysWarning"] = 2 if sys_warning else 0
 
   elif car_fingerprint == CAR.GENESIS:
     # This field is actually LdwsActivemode
@@ -49,10 +49,10 @@ def create_lkas11(packer, frame, car_fingerprint, apply_steer, steer_req,
     values["CF_Lkas_FcwOpt_USM"] = 2 if enabled else 1
     values["CF_Lkas_LdwsOpt_USM"] = 2
     values["CF_Lkas_FcwOpt_USM"] = 2 if enabled else 1
-    values["CF_Lkas_SysWarning"] = 4 if sys_warning else 0
+    values["CF_Lkas_SysWarning"] = 2 if sys_warning else 0
 
   if FEATURES["use_ldws"]:
-    values["CF_Lkas_LdwsOpt_USM"] = 3
+    values["CF_Lkas_LdwsOpt_USM"] = 2
 
   dat = packer.make_can_msg("LKAS11", 0, values)[2]
 
@@ -96,7 +96,7 @@ def create_lfahda_mfc(packer, enabled, hda_set_speed=0):
 def create_mdps12(packer, frame, mdps12):
   values = copy.copy(mdps12)
   values["CF_Mdps_ToiActive"] = 0
-  values["CF_Mdps_ToiUnavail"] = 1
+  values["CF_Mdps_ToiUnavail"] = 0
   values["CF_Mdps_MsgCount2"] = frame % 0x100
   values["CF_Mdps_Chksum2"] = 0
 
