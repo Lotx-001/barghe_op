5bdc5717e874f90a9ef6f91d2927461a3f28bf6a
Lotx-001 <72420846+Lotx-001@users.noreply.github.com>
2021-08-15 18:19:40 +0300

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -29,9 +29,9 @@ class CarControllerParams:
 
 # Steer torque limits
 class SteerLimitParams:
-  STEER_MAX = 550   # 550 is the max, 255 is stock
-  STEER_DELTA_UP = 3 
-  STEER_DELTA_DOWN = 6 
+  STEER_MAX = 455  
+  STEER_DELTA_UP = 4 
+  STEER_DELTA_DOWN = 7 
   STEER_DRIVER_ALLOWANCE = 50
   STEER_DRIVER_MULTIPLIER = 2
   STEER_DRIVER_FACTOR = 1
